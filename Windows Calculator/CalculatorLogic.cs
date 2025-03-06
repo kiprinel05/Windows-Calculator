4c858fe53f6692b03b5dd9a12ab377e0c94f2f3e
Ciprian D <147750749+kiprinel05@users.noreply.github.com>
2025-03-06 19:25:32 +0000

    initial version

---
+++ Windows Calculator/CalculatorLogic.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace WPFCalculator.Models
+{
+    public class CalculatorLogic
+    {
+        private double _currentValue = 0;
+        private double _memoryValue = 0;
+        private string _currentOperator = "";
+        private bool _isNewInput = true;
+        private MemoryManager memoryManager = new MemoryManager();
+        public string CurrentOperator { get; private set; } = "";
+        public string CurrentDisplay { get; private set; } = "0";
+
+        public void AddDigit(string digit)
+        {
+            if (_isNewInput)
+            {
+                CurrentDisplay = digit;
+                _isNewInput = false;
+            }
+            else
+            {
+                CurrentDisplay += digit;
+            }
+        }
+
+        public void SetOperator(string op)
+        {
+            CurrentOperator = op;
+            if (!string.IsNullOrEmpty(_currentOperator))
+                Calculate();
+
+            _currentValue = double.Parse(CurrentDisplay);
+            _currentOperator = op;
+            _isNewInput = true;
+        }
+
+        public void Calculate()
+        {
+            double newValue = double.Parse(CurrentDisplay);
+            switch (_currentOperator)
+            {
+                case "+": _currentValue += newValue; break;
+                case "-": _currentValue -= newValue; break;
+                case "*": _currentValue *= newValue; break;
+                case "/":
+                    if (newValue != 0)
+                        _currentValue /= newValue;
+                    else
+                        CurrentDisplay = "Eroare";
+                    break;
+            }
+            CurrentDisplay = _currentValue.ToString();
+            _currentOperator = "";
+            _isNewInput = true;
+        }
+
+        public void Clear()
+        {
+            _currentValue = 0;
+            _currentOperator = "";
+            CurrentDisplay = "0";
+            _isNewInput = true;
+        }
+        public void CalculatePercentage()
+        {
+            if (double.TryParse(CurrentDisplay, out double value))
+            {
+                CurrentDisplay = (value / 100).ToString();
+            }
+        }
+
+        public void SquareRoot()
+        {
+            if (double.TryParse(CurrentDisplay, out double value) && value >= 0)
+            {
+                CurrentDisplay = Math.Sqrt(value).ToString();
+            }
+        }
+
+        public void Square()
+        {
+            if (double.TryParse(CurrentDisplay, out double value))
+            {
+                CurrentDisplay = (value * value).ToString();
+            }
+        }
+
+        public void Reciprocal()
+        {
+            if (double.TryParse(CurrentDisplay, out double value) && value != 0)
+            {
+                CurrentDisplay = (1 / value).ToString();
+            }
+        }
+
+        public void Negate()
+        {
+            if (double.TryParse(CurrentDisplay, out double value))
+            {
+                CurrentDisplay = (-value).ToString();
+            }
+        }
+        public void MemoryClear()
+        {
+            memoryManager.MemoryClear();
+        }
+
+        public void MemoryRecall()
+        {
+            string display = memoryManager.MemoryView();
+            if (!string.IsNullOrEmpty(display))
+            {
+                CurrentDisplay = display;
+            }
+        }
+
+        public void MemoryAdd()
+        {
+            if (double.TryParse(CurrentDisplay, out double value))
+            {
+                memoryManager.MemoryAdd(value);
+            }
+        }
+
+        public void MemorySubtract()
+        {
+            if (double.TryParse(CurrentDisplay, out double value))
+            {
+                memoryManager.MemorySubtract(value);
+            }
+        }
+
+        public void MemoryStore()
+        {
+            if (double.TryParse(CurrentDisplay, out double value))
+            {
+                memoryManager.MemoryStore(value);
+            }
+        }
+
+        public string MemoryView()
+        {
+            return memoryManager.MemoryView();
+        }
+        public enum CalculatorMode
+        {
+            Standard,
+            Programmer
+        }
+
+        private CalculatorMode currentMode = CalculatorMode.Standard;
+
+        public void SetMode(CalculatorMode mode)
+        {
+            currentMode = mode;
+        }
+
+    }
+}
